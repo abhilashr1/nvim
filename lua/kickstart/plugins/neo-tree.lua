b9f6e47d12effcd0ce8d7eaa451ba0616156fdfb
Abhilash <abhilash@linkby.com>
2024-06-22 12:51:17 +0700

    Add filetree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,6 +14,18 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          '.git',
+          '.DS_Store',
+          'thumbs.db',
+        },
+        never_show = {},
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
